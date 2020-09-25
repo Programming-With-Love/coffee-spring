bc0283de9b412d817b89907ef6273fa26a25910e
zido <wuhongxu1208@gmail.com>
2020-09-25 09:25:14 +0800

    :typo: 修复异常返回对象被包装两层的问题

--- coffee-autoconfigures/coffee-autoconfigure-web/src/main/java/site/zido/coffee/autoconfigure/web/CoffeeErrorMvcAutoConfiguration.java
+++ coffee-autoconfigures/coffee-autoconfigure-web/src/main/java/site/zido/coffee/autoconfigure/web/CoffeeErrorMvcAutoConfiguration.java
@@ -1,19 +1,32 @@
 package site.zido.coffee.autoconfigure.web;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.SearchStrategy;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import site.zido.coffee.autoconfigure.web.basic.CoffeeErrorAttributes;
 import site.zido.coffee.mvc.rest.HttpResponseBodyConfiguration;
 import site.zido.coffee.mvc.rest.HttpResponseBodyFactory;
+import site.zido.coffee.mvc.rest.OriginalResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableConfigurationProperties({ServerProperties.class})
@@ -31,5 +44,24 @@ class CoffeeErrorMvcAutoConfiguration {
     @Bean
     public CoffeeErrorAttributes errorAttributes(HttpResponseBodyFactory factory) {
         return new CoffeeErrorAttributes(this.serverProperties.getError().isIncludeException(), factory);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(value = ErrorController.class, search = SearchStrategy.CURRENT)
+    public BasicErrorController basicErrorController(ErrorAttributes errorAttributes,
+                                                     ObjectProvider<ErrorViewResolver> errorViewResolvers) {
+        return new BasicErrorController(errorAttributes, this.serverProperties.getError(),
+                errorViewResolvers.orderedStream().collect(Collectors.toList())) {
+            @RequestMapping
+            @OriginalResponse
+            public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+                HttpStatus status = getStatus(request);
+                if (status == HttpStatus.NO_CONTENT) {
+                    return new ResponseEntity<>(status);
+                }
+                Map<String, Object> body = getErrorAttributes(request, isIncludeStackTrace(request, MediaType.ALL));
+                return new ResponseEntity<>(body, status);
+            }
+        };
     }
 }
--- coffee-modules/coffee-webmvc/src/main/java/site/zido/coffee/mvc/rest/GlobalResultHandler.java
+++ coffee-modules/coffee-webmvc/src/main/java/site/zido/coffee/mvc/rest/GlobalResultHandler.java
@@ -36,7 +36,6 @@ public class GlobalResultHandler implements ResponseBodyAdvice<Object> {
                                   Class<? extends HttpMessageConverter<?>> selectedConverterType,
                                   ServerHttpRequest request,
                                   ServerHttpResponse response) {
-        //TODO 跳过/error错误处理，这与errorController冲突
         OriginalResponse methodAnnotation = returnType.getMethodAnnotation(OriginalResponse.class);
         if (methodAnnotation != null || returnType.getDeclaringClass().getAnnotation(OriginalResponse.class) != null) {
             return body;
